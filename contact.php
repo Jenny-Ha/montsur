2aa1f64aaf8fd323681817ef1b77ee2f334e7730
Jenny Ha <jey.hah@gmail.com>
2020-08-21 20:25:27 -0500

    contact php remove parse

--- contact.php
+++ contact.php
@@ -5,11 +5,11 @@
     $subject = "Web Montsur - Contacto";
 
     //Correo que se enviará
-    $name 	 = stripslashes($_POST['name']);
-	$email 	 = trim($_POST['email']);
-    $phone = trim($_POST['phone']);
-    $company = trim($_POST['subject']);
-    $message = stripslashes($_POST['message']);
+    $name 	 = $_POST['name'];
+	$email 	 = $_POST['email'];
+    $phone = $_POST['phone'];
+    $company = $_POST['subject'];
+    $message = $_POST['message'];
 
     $headers  = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
@@ -18,10 +18,10 @@
     $headers .= "X-Mailer: PHP/" . phpversion();
     $headers .= "X-Priority: 1" . "\r\n";
 
-    $mensajeCompleto = $message . '\nAtentamente: ' . $name . $company;
+    $mensajeCompleto = $message . '\nAtentamente: ' . $name . $company . $email . $phone;
 
     if(mail($to, $subject, $mensajeCompleto, $headers)) {
-        
+       
         echo "<script>alert('Su mensaje se envió correctamente')</script>";
     } else {
         echo "<script>alert('Failed envio')</script>";
